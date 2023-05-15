b677f6098a84b8db917e64e242123ae57b1e8352
apprakash <101494351+apprakash@users.noreply.github.com>
2023-05-15 11:35:10 +0530

    Pdf loader Added

--- src/pages/api/summary.ts
+++ src/pages/api/summary.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { inngest } from "./inngest";
 import axios from "axios";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { OpenAI } from "langchain/llms/openai";
 
 type Data = {
   url: string;
@@ -14,7 +16,14 @@ export default async function handler(
   const response = await axios.get(req.body.url, {
     responseType: "arraybuffer",
   });
-  console.log(response);
+
+  const pdfData = response.data;
+  const pdfBlob = new Blob([pdfData], { type: "application/pdf" });
+  const loader = new PDFLoader(pdfBlob);
+  const docs = await loader.load();
+  const envir = process.env.OPENAI_API_KEY;
+  const model = new OpenAI({ openAIApiKey: envir, temperature: 0 });
+  console.log(docs);
 
   res.status(200).json({ url: req.body.url, text: "Hello World!!" });
 }
