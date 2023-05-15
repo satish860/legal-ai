9f5d5ba4adef067e0e7b7b119cbbe20b473c6b32
apprakash <101494351+apprakash@users.noreply.github.com>
2023-05-15 16:57:26 +0530

    Vercel Edge added

--- src/pages/api/map.ts
+++ src/pages/api/map.ts
@@ -1,7 +1,8 @@
 import { LLMChain } from "langchain/chains";
 import { OpenAI } from "langchain/llms/openai";
 import { PromptTemplate } from "langchain/prompts";
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
+import { json } from "stream/consumers";
 
 type Data = {
   summary: string;
@@ -10,21 +11,25 @@ type Data = {
 const template = `Write a concise summary of the following:
 "{text}"
 CONCISE SUMMARY:`;
+const envir = process.env.OPENAI_API_KEY;
+
+const model = new OpenAI({ openAIApiKey: envir, temperature: 0 });
+
+export const config = {
+  runtime: "edge",
+};
 
 export const DEFAULT_PROMPT = new PromptTemplate({
   template,
   inputVariables: ["text"],
 });
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  const text = req.body.text;
-  const envir = process.env.OPENAI_API_KEY;
-  const model = new OpenAI({ openAIApiKey: envir, temperature: 0 });
-  const chain = new LLMChain({ llm: model, prompt: DEFAULT_PROMPT });
-  const result = await chain.call({ text: text });
-//   console.log(result);
-  res.status(200).json({ summary: result.text });
-}
+export default async (request: NextRequest) => {
+  const { text } = await request.json();
+
+  const promptText = await DEFAULT_PROMPT.format({ text });
+  const result = await model.call(promptText);
+  return NextResponse.json({
+    summary: result,
+  });
+};
